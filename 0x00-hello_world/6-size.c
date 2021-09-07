4e6365e57c9b253f25bc41281d040c287df2a64d
Mdesigns Code <marlonbaatjes830@gmail.com>
2021-09-07 10:55:33 -0700

    Hello World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+        printf("The size of type 'int' is: %lu bytes.\n", sizeof(int));
+        printf("The size of type 'char' is: %lu bytes.\n", sizeof(char));
+        printf("The size of type 'float' is: %lu bytes.\n", sizeof(float));
+        return (0);
+}
