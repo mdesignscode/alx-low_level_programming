202ff0134286dbf502258a269882bbd1d8bc6838
Marlon <83790170+mdesignscode@users.noreply.github.com>
2021-09-08 13:23:53 -0400

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  * main - Prints string
- * Description - prints 'with proper grammar, but the outcome is a 
- * piece of art,'
+ * Description - print: with proper grammar, but the outcome is a piece of art,
  */
 int main(void)
 {
