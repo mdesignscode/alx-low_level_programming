0da961e3f78f99ee5168710c840d675572a8aac7
Mdesigns Code <marlonbaatjes830@gmail.com>
2021-09-08 09:55:23 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-/*
+/**
  * main - Prints string
  * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+}	
