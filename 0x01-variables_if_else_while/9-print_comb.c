eaecf339897dde8cd2efc4a8d0398d31a39144c6
Mdesigns Code <marlonbaatjes830@gmail.com>
2021-09-15 07:15:11 -0700

    base 10 digits seperated by commas and spaces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,14 @@
 int main(void)
 {
 	int a;
+
 	for (a = 48; a < 58; a++)
 	{
 		putchar(a);
 		if (a != 57)
 		{
-			putchar(44);
-			putchar(32);
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
