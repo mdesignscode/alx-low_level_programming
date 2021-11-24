3d2cfde4a5dd97dea701bd22c62029e26cea50a2
Mdesigns Code <marlonbaatjes830@gmail.com>
2021-11-24 06:06:45 -0800

    remove executable files

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,32 +6,35 @@
  */
 int main(void)
 {
-	int d00, d01, d12, d13, n;
+	int d00, d01, d12, d13;
+	int sep1 = 0;
+	int sep2 = 0;
 
-	n = 0;
-	d00 = 48;
-	while (d00 < 58)
+	d00 = 0;
+	while (d00 < 10)
 	{
-		d01 = 48;
-		while (d01 < 57)
+		d01 = 0;
+		while (d01 < 10)
 		{
-			d12 = 48;
-			while (d12 < 58)
+			d12 = 0;
+			while (d12 < 10)
 			{
-				d13 = d01 + 1;
-				while (d13 < 58)
+				d13 = 0;
+				while (d13 < 10)
 				{
-					if (n > 0)
+					if (!(d00 == d13) && (d01 == d12))
 					{
-						putchar(44);
+						putchar(sep1);
+						putchar(sep2);
+						putchar(48 + d00);
+						putchar(48 + d01);
 						putchar(32);
+						putchar(48 + d12);
+						putchar(48 + d13);
+						sep1 = ',';
+						sep2 = ' ';
 					}
-					putchar(d00);
-					putchar(d01);
-					putchar(32);
-					putchar(d12);
-					putchar(d13);
-					n++;
+
 					d13++;
 				}
 				d12++;
