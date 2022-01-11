b46d3933527fe7fd2f83ceb59df376b5da259dbe
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-01-11 06:41:54 -0800

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *p;
 
-	p = malloc(sizeof(unsigned int) * size);
+	p = malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 		p[i] = c;
