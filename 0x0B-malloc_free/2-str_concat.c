7d963c8f21ab745198a7bd15974a8fcffd244f5f
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-01-12 01:40:52 -0800

    malloc && free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,21 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
+ * _strlen - counts the length of a string.
+ * @s: string to be counted.
+ *
+ * Return: length of string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+
+	return (len);
+}
+/**
  * str_concat - concatenates two strings.
  * @s1: string 1.
  * @s2: string 2.
