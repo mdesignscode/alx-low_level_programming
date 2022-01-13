3a39f98f50fc600dcec6d1c711d7acec52156acc
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-01-12 17:27:12 -0800

    string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,23 +24,23 @@ int _strlen(char *s)
  */
 char *str_concat(char *s1, char *s2)
 {
-	int l1, l2, l3, i;
+	int l1, l2, l3, i, j;
 	char *p;
 
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
-	l3 = l1 + l2 + l2 + 1;
+	l3 = l1 + l2 + 1;
+
 	p = malloc(sizeof(char) * l3);
-	if ((!s1 || !s2) || (!s1 && !s2))
+
+	if (!p || !s1 || !s2)
 		return (NULL);
-	else if (!p)
-		return (NULL);
-	for (i = 0; i < l1; i++)
+
+	for (i = 0; s1[i]; i++)
 		p[i] = s1[i];
-	for (i = 0; i < l2 + l2; i++)
-	{
-		p[l1 + i] = s2[i];
-	}
+	for (j = 0; s2[j]; j++)
+		p[i++] = s2[j];
+
 	p[i] = '\0';
 	return (p);
 }
