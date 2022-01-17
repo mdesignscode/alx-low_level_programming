398978e9bc212d141087472d22ff0aafbbcaaba0
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-01-17 04:25:13 -0800

    more malloc && free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,63 +1,49 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * _strlen - counts the length of a string.
- * @s: string to be counted.
+ * _strlen -calculate the length of string
+ * @s: the string
  *
- * Return: length of string.
+ * Return: length
  */
 int _strlen(char *s)
 {
-	int len = 0;
+	char *p = s;
 
-	while (s[len])
-		len++;
-
-	return (len);
+	while (*s)
+		s++;
+	return (s - p);
 }
+
 /**
- * string_nconcat - concatenates two strings.
- * Description: If n is greater or equal to the length of s2
- * then use the entire string s2
- * @s1: string 1.
- * @s2: string 2.
- * @n: number of bytes to appended.
- *
- * Return: a pointer to the new string of n bytes appended.
+ * string_nconcat - concatenating two string
+ * @s1: string1
+ * @s2: string2
+ * @n: integer
+ * Return: concat
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, l1, l2, l3, index = 0;
-	char *p;
+	unsigned int i, j, s, l1, l2;
+	char *ptr;
 
-	if (!s1)
+	if (s1 == NULL)
 		s1 = "";
-	else if (!s2)
+	if (s2 == NULL)
 		s2 = "";
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
-	if (n >= l2)
-	{
+	if (n > l2)
 		n = l2;
-		l3 = l2 + 1;
-		p = malloc(sizeof(char) * l3);
-		if (!p)
-			return (NULL);
-		for (j = 0; j < n; j++)
-			p[j] = s2[j];
-		p[j] = '\0';
-	}
-	else
-	{
-		l3 = l1 + n + 1;
-		p = malloc(sizeof(char) * l3);
-		if (!p)
-			return (NULL);
-		for (i = 0; s1[i]; i++)
-			p[index++] = s1[i];
-		for (j = 0; j < n; j++, index++)
-			p[l1 + j] = s2[j];
-		p[index] = '\0';
-	}
-	return (p);
+	s = l1 + n;
+	ptr = malloc(sizeof(char) * s + 1);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < l1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < l2; j++, i++)
+		ptr[i] = s2[j];
+	ptr[i] = 0;
+	return (0);
 }
