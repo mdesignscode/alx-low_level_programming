2faae3a3748e58d4506d9cad2b9a54d394151f7f
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-01-30 02:21:41 -0800

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (!array || !size || !action)
-		return (NULL);
+		return;
 	while (size-- && array)
 		action(*array++);
 }
