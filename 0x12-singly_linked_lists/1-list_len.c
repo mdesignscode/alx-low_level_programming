280e2e909ac893bab1745dea8ee7294f2bab0772
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-02-15 09:59:09 -0800

    singly linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,15 +7,14 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t count;
+	int count;
 
 	count = 0;
-	while (h->next)
+	while (h != NULL)
 	{
-		h = h->next;
 		count++;
+		h = h->next;
 	}
-	h = h->next;
-	count++;
+
 	return (count);
 }
