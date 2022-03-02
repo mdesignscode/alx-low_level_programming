8ba7493c945b8f14577b3b96466516794dfdd764
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-02 09:19:35 -0800

    file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,24 +8,19 @@
  */
 int main(int argc, char **argv)
 {
-	int file_from, file_to, reaD, writE, index, close_from, close_to;
-	charptr buffer;
-	charptr error97 = "Usage: cp file_from file_to\n";
-	charptr error98 = "Error: Can't read from file ";
-	charptr error99 = "Error: Can't write to ";
-	charptr error100 = "Error: Can't close ";
-	charptr newline = "\n";
+	int file_from, file_to, reaD, writE, close_from, close_to;
+	char buffer[1024];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, error97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_WRONLY | O_TRUNC);
 	if (file_to == -1)
-		file_to = open(argv[2], O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	buffer = malloc(sizeof(char) * 1024);
+		file_to = open(argv[2], O_WRONLY | O_CREAT, mode);
 	if (file_from == 3)
 	{
 		while ((reaD = read(file_from, buffer, 1024)) != 0)
@@ -33,32 +28,22 @@ int main(int argc, char **argv)
 	}
 	if (file_from == -1 || reaD == -1)
 	{
-		dprintf(STDERR_FILENO, error98);
-		dprintf(STDERR_FILENO, argv[1]);
-		dprintf(STDERR_FILENO, newline);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1 || writE == -1)
 	{
-		dprintf(STDERR_FILENO, error99);
-		dprintf(STDERR_FILENO, argv[2]);
-		dprintf(STDERR_FILENO, newline);
-		exit(99);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	close_from = close(file_from);
+	close_to = close(file_to);
 	if (close_from == -1)
 	{
-		dprintf(STDERR_FILENO, error100);
-		_putchar(file_from);
-		dprintf(STDERR_FILENO, newline);
-		exit(100);
+		dprintf(2, "Error: Can't close %d\n", file_from), exit(100);
 	}
 	else if (close_to == -1)
 	{
-		dprintf(STDERR_FILENO, error100);
-		_putchar(file_to);
-		dprintf(STDERR_FILENO, newline);
-		exit(100);
+		dprintf(2, "Error: Can't close %d\n", file_to), exit(100);
 	}
 	return (0);
 }
