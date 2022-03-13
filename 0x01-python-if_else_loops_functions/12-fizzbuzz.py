460a0e2dafcc4b1d4580f54ae866b34e8ffa542a
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-13 15:56:26 -0700

    prep question

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-For multiples of three print Fizz instead of the number and for multiples of five print Buzz.
-For numbers which are multiples of both three and five print FizzBuzz.
-Prototype: def fizzbuzz():
-Each element should be followed by a space
-"""
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0) and (i % 5 == 0):
