eae6da3049ff5be97e9c4994f95ca8b728a858af
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-16 01:26:17 -0700

    import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         print('{} argument:'.format(1))
         print('1: {}'.format(argv[1]))
     elif argc == 1:
-            print('{} arguments.'.format(0))
+        print('{} arguments.'.format(0))
     else:
         print('{} arguments:'.format(argc - 1))
         for i in range(1, argc):
