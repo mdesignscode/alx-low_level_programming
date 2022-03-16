954dda602466de2032100983f537ee480ddcc098
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-16 01:41:55 -0700

    import modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,10 @@
 from sys import argv
 from calculator_1 import add, mul, div, sub
 
-argc = len(argv)
+argc = len(argv) - 1
 operators = '+-*/'
 
-if argc != 4:
+if argc != 3:
     print('Usage: ./100-my_calculator.py <a> <operator> <b>')
     exit(1)
 
