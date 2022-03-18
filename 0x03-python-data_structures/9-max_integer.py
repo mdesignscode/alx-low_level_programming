b4ff85ac977bb8e21ae5f8a170e2eaec1867ef8b
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-18 12:45:05 -0700

    import modules

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,8 @@ Finds the biggest integer of a list.
 Return: None if list is empty, else the biggest integer.
 """
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+            return None
     biggest = 0
     for element in my_list:
         if element > biggest:
