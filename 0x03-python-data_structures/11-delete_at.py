f610b381673460ba365666e9f474a877db292d6d
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-18 12:31:34 -0700

    import modules

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""
-Deletes the item at a specific position in a list.1
-Return: List without item, or
-Original list if idx is negative or out of range
-"""
+
 def delete_at(my_list=[], idx=0):
+    """
+    Deletes the item at a specific position in a list.1
+    Return: List without item, or
+    Original list if idx is negative or out of range
+    """
     if idx >= 0 and idx < len(my_list):
         del my_list[idx]
     return my_list
