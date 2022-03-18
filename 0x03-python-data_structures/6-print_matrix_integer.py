731ddfbe1ebbba33522436662d9b2c9e00efa59c
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-18 13:06:47 -0700

    import modules

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,13 +8,13 @@ Return: None
 def print_matrix_integer(matrix=[[]]):
     ll = len(matrix)
 
-    if ll == 1:
-        print('{}'.format())
+    if ll == 1 or ll == 0:
+        print()
 
     for i in range(ll):
         for j in range(len(matrix[i])):
             print('{}'.format(matrix[i][j]), end='')
             if j == ll - 1:
-                print('{}'.format())
+                print()
             else:
-                print('{}'.format(' '), end='')
+                print(' ', end='')
---
+++ 0x03-python-data_structures/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+matrix = []
+
+
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
