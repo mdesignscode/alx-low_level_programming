aa8d5c68ec5140e8a40300f45537e9ca5918f584
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-18 12:25:02 -0700

    import modules

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,8 +5,6 @@ Return: List without item, or
 Original list if idx is negative or out of range
 """
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
-        return my_list
-    else:
+    if idx >= 0 and idx < len(my_list):
         del my_list[idx]
-        return my_list
+    return my_list
