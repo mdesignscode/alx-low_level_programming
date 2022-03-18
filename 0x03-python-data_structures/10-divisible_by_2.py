a49a5c394a792d7177c6f5baafac398029fd3ed6
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-18 12:47:22 -0700

    import modules

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,10 +5,13 @@ Return: a new list with True or False,
 depending on whether the integer at the same position
 in the original list is a multiple of 2
 """
+
+
 def is_div(element):
     if element % 2 == 0:
         return True
     return False
+
 
 def divisible_by_2(my_list=[]):
     new_list = []
--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,8 @@ Deletes the item at a specific position in a list.1
 Return: List without item, or
 Original list if idx is negative or out of range
 """
+
+
 def delete_at(my_list=[], idx=0):
     if idx >= 0 and idx < len(my_list):
         del my_list[idx]
