ebc8a33dee2e392955bfb6874526297d73c52f23
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-22 03:26:10 -0700

    more data structures

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,13 +6,4 @@ Return: a set
 
 
 def only_diff_elements(set_1, set_2):
-    new_set = set()
-    for element_1 in set_1:
-        for element_2 in set_2:
-            new_set.add(element_2)
-            if element_1 == element_2:
-                new_set.remove(element_2)
-        new_set.add(element_1)
-        if element_1 == element_2:
-            new_set.remove(element_1)
-    return new_set
+    return set_1 ^ set_2
