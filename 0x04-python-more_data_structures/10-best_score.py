d707a81fefcd6855d29068f4003b2365611a5180
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-03-22 03:13:34 -0700

    more data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
 best_score: returns a key with the biggest integer value.
-Return: None if no score found
+Return: None if no score found else the highest score
 """
 
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or a_dictionary == {}:
         return None
 
     return max(a_dictionary)
