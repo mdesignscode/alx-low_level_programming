3833231bfb44039426e4b5bc6e868f2e746e96b5
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-04-07 14:49:23 -0700

    correcting logic errors

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		nodes++;
 		temp = temp->next;
 	}
-	if (idx > nodes)
+	if (idx > nodes || !temp)
 		return NULL;
 
 	temp = *h;
