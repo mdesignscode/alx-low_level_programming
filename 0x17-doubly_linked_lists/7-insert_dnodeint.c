eba46dd0bea61d33a847484a7c688a4f89f06de6
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-04-07 14:47:59 -0700

    correcting logic errors

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	d_l_pointer new_node, temp;
-	unsigned int i;
+	unsigned int i, nodes = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
@@ -21,16 +21,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = NULL;
 
 	temp = *h;
-	if (temp)
+	while (temp)
 	{
-		for (i = 0; i < idx - 1; i++)
-		{
-			temp = temp->next;
-		}
-		new_node->next = temp->next;
-		temp->next = new_node;
+		nodes++;
+		temp = temp->next;
 	}
-	else
-		return (NULL);
+	if (idx > nodes)
+		return NULL;
+
+	temp = *h;
+	for (i = 0; i < idx - 1; i++)
+		temp = temp->next;
+	new_node->next = temp->next;
+	temp->next = new_node;
 	return (new_node);
 }
