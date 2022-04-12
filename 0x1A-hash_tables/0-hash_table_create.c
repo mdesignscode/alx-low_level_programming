239a8377989cc1aac6a9c4bc0fc9834cd3dd32e2
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-04-12 07:27:51 -0700

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_pointer table;
 	unsigned long int index;
+	node_pointer aux;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
@@ -23,6 +24,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
+	aux = malloc(sizeof(hash_node_t));
+	if (!aux)
+		return (NULL);
+	aux->key = 0;
+	aux->next = 0;
+	aux->value = 0;
 	for (index = 0; index < size; index++)
 	{
 		table->array[index] = NULL;
